863410bf77a45db3630a7f68ac89937c68b6cd7a
sunwengpeng6688 <15249679989>
2019-04-28 02:56:51 +0800

    新增

--- src/main/java/com/cn/test2.java
+++ src/main/java/com/cn/test2.java
@@ -4,16 +4,26 @@ public class test2 {
 
 	public static void main(String[] args) {
 
-	System.out.println("新的修改");
-
+	System.out.println("新的修改1");
+	System.out.println("新的修改1");
+	System.out.println("新的修改1");
+	System.out.println("新的修改1");
+	System.out.println("新的修改1");
+	System.out.println("新的修改1");
+	System.out.println("新的修改1");
+	System.out.println("新的修改1");
+	for (int i = 0; i < 3; i++) {
+		System.out.println("i");
 		
+		if(i==0) {
+			System.out.println("到我这里该处来了");
+			break;
+			
+			
+		}
 		
+	}
 
-               System.out.println("mytest1");
-		System.out.println("xinze");
-
-		System.out.println("mytest1");
-		System.out.println("mytest1");
 
 
 	}
