ba7986751f92601bbaf8203a27cf5674a5dd61f2
sunwengpeng6688 <50048105+sunwengpeng6688@users.noreply.github.com>
2019-04-28 02:15:26 +0800

    Update test2.java

--- src/main/java/com/cn/test2.java
+++ src/main/java/com/cn/test2.java
@@ -3,8 +3,9 @@ package com.cn;
 public class test2 {
 
 	public static void main(String[] args) {
-		System.out.println("mytest1");
-
+		System.out.println("myte");
+		
+               System.out.println("mytest1");
 	}
 
 }
